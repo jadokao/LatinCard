761b1dbbf7d7db8ce1df7fe8c3fa53dd33d102de
jacky <jackykao528@outlook.com>
2022-03-13 20:11:19 +0800

    basic graphql

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express')
 var router = express.Router()
+var { graphqlHTTP } = require('express-graphql')
+var { buildSchema } = require('graphql')
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
+var root = { hello: () => 'Hello world!' }
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
 	res.render('index', { title: 'Express' })
 })
+
+router.get(
+	'/graphql',
+	graphqlHTTP({
+		schema: schema,
+		rootValue: root,
+		graphiql: true
+	})
+)
 
 module.exports = router
