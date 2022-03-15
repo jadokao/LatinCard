d8290e2830f54c04edfd54c607998111fd96ce53
jacky <jackykao528@outlook.com>
2022-03-15 20:11:06 +0800

    fix cors by axios and route

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios')
 const router = express.Router()
 
 const userControllers = require('../controllers/userControllers.js')
@@ -6,5 +7,10 @@ const cardControllers = require('../controllers/cardControllers.js')
 
 /* GET home page. */
 router.get('/', (req, res) => res.render('index', { title: 'Test in routes.js' }))
+router.get('/search', (req, res) => {
+	axios.get(`https://latinwordnet.exeter.ac.uk/api/lemmas/${req.query.word}/n/`).then(response => {
+		return res.json(response.data.results)
+	})
+})
 
 module.exports = router
