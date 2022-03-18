5a90f9ff6b511f2da778dc04b4a6339e697045cb
jacky <jackykao528@outlook.com>
2022-03-18 21:09:06 +0800

    basic sign up controller with mutation

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,6 @@
-const { User, Card } = require('../models')
+const { User } = require('../models')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userController = {
 	findUser: async account => {
@@ -11,6 +13,25 @@ const userController = {
 			return user
 		})
 		return result
+	},
+	signUp: async (name, account, password) => {
+		if (!name || !account || !password) throw new Error('請完整填寫')
+		const result = await User.findOne({ where: { account: account } }).then(user => {
+			if (!user) {
+				// account找不到 >> 還沒註冊過
+				return User.create({
+					account: account,
+					name: name,
+					password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+				})
+					.then(user => {
+						return user
+					})
+					.catch(err => console.log(err))
+			}
+			throw new Error('已經重複註冊')
+		})
+		return result.dataValues
 	}
 }
 
