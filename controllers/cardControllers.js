1607e56c8dc344ea71d964d0e4a7e95dbdb99121
jacky <jackykao528@outlook.com>
2022-03-18 22:20:52 +0800

    basic post card controller with mutation

--- controllers/cardControllers.js
+++ controllers/cardControllers.js
@@ -1,7 +1,17 @@
-const db = require('../models')
-const User = db.User
-const Card = db.Card
+const { User, Card, Book } = require('../models')
 
-const cardController = {}
+const cardController = {
+	postCard: async (input, id) => {
+		const { ch, en, la, partOfSpeech } = input
+		if (!ch || !en || !la || !partOfSpeech) throw new Error('請完整填寫')
+
+		const result = await Card.create({ ch, en, la, partOfSpeech }).then(card => {
+			console.log(card.dataValues)
+			return card.dataValues
+		})
+		await Book.create({ CardId: result.id, UserId: id })
+		return result
+	}
+}
 
 module.exports = cardController
