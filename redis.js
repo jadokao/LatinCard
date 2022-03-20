7a060da0b94eb6139437b37dc44b035aa4037ac1
jacky <jackykao528@outlook.com>
2022-03-21 01:38:26 +0800

    fix redis setting

--- redis.js
+++ redis.js
@@ -1,5 +1,9 @@
 const redis = require('redis')
-const redisClient = redis.createClient()
+const redisClient = redis.createClient({
+	url: 'redis://redis:6379',
+	host: 'redis-server',
+	port: 6379
+})
 
 redisClient.on('connect', () => console.log('Connected to Redis!'))
 redisClient.on('error', err => console.log('Redis Client Error', err))
